7743aa693c9b8f395593c3178d8c835407ff4b72
smyshir <smyshir987@gmail.com>
2025-10-10 05:31:12 +0300

    Изменения в file1 и file2 в ветке dev3

--- person1/file1.py
+++ person1/file1.py
@@ -1,3 +1,6 @@
 def greet(): print('Hello from file1')
 # Изменение во второй копии (person2)
 # Изменение из person3 (ветка dev3)
+
+
+#izmeneniye dlya patchey
--- person1/file2.py
+++ person1/file2.py
@@ -1,2 +1,6 @@
 def add(a, b): return a + b
 # Изменение в первой копии (person1)
+
+
+
+#boba_biba_patch
