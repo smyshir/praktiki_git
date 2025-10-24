28b4795ee98da26b1942c79c8a660f8e92b05450
smyshir <smyshir987@gmail.com>
2025-10-24 06:33:52 +0300

    Добавлена функция feature_sum и тест для неё

---
+++ feature_sum.py
@@ -0,0 +1,5 @@
+def feature_sum(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    print('Result:', feature_sum(3, 4))
---
+++ test_feature_sum.py
@@ -0,0 +1,4 @@
+from feature_sum import feature_sum
+
+def test_feature_sum():
+    assert feature_sum(2, 3) == 5
